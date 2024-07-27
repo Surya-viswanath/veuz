9c070c5efb338f6cb1068c52283e1d301a1d9c05
Surya-viswanath <suryaviswanath22@gmail.com>
2024-07-27 13:54:01 +0530

    loading

--- my-app/src/Menu.jsx
+++ my-app/src/Menu.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { FaSearch } from "react-icons/fa";
+import './Menu.css'
 
 function Menu() {
   return (
@@ -10,13 +11,13 @@ function Menu() {
         
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mx-auto" style={{clor:'white'}}>
-            <Nav.Link href="#features">Home</Nav.Link>
+          <Nav className="mx-auto" >
+            <Nav.Link href="#features" style={{color:'white',fontWeight:'500'}}>Home</Nav.Link>
             {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
            
           </Nav>
           <Nav style={{gap:'5vh'}}>
-          <NavDropdown title="Package" id="collapsible-nav-dropdown"  >
+          <NavDropdown title="Package" id="collapsible-nav-dropdown"  className='whitecl'>
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -27,7 +28,7 @@ function Menu() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Destination" id="collapsible-nav-dropdown">
+            <NavDropdown title="Destination" id="collapsible-nav-dropdown" className='whitecl'>
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -38,7 +39,7 @@ function Menu() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Blog" id="collapsible-nav-dropdown">
+            <NavDropdown title="Blog" id="collapsible-nav-dropdown" className='whitecl'>
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -49,8 +50,8 @@ function Menu() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#deets">Contact</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
+            <Nav.Link href="#deets" style={{color:'white',fontWeight:'400'}}>Contact</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes" style={{color:'white',fontWeight:'400'}}>
             <FaSearch />
             </Nav.Link>
           </Nav>
