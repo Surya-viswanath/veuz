e205573b1990811a8947e0c149fd9bd3690cf32e
Surya-viswanath <suryaviswanath22@gmail.com>
2024-07-27 22:43:42 +0530

    finishing stage

--- my-app/src/Animation.jsx
+++ my-app/src/Animation.jsx
@@ -1,70 +1,58 @@
 
-// corrected
-
 // import React, { useEffect, useState } from 'react';
 // import './animation.css';
 // import img1 from './images/img1.png';
-//  import img2 from './images/img2.png';
+// import img2 from './images/img2.png';
 // import img3 from './images/img3.png';
-
+// import { Col, Row } from 'react-bootstrap'
 // const Animation = () => {
-//     const [windowState, setWindowState] = useState('open');
+//     const [windowState, setWindowState] = useState('closed');
 
-//   useEffect(() => {
-//     const animationInterval = 12000; // 12 seconds for the complete animation cycle
-//     const step = animationInterval / 6; // 6 steps (each 16.67%)
+//     useEffect(() => {
+//         const animationInterval = 12000; // 12 seconds for the complete animation cycle
+//         const step = animationInterval / 6; // 6 steps
 
-//     const interval = setInterval(() => {
-//       setTimeout(() => {
-//         setWindowState('open');
-//       }, step * 1); // At 16.67%
+//         const interval = setInterval(() => {
+//             setWindowState('open');
+//             // setTimeout(() => setWindowState('closed'), step);
+//             // setTimeout(() => setWindowState('idle'), step * 2);
+//             // setTimeout(() => setWindowState('open'), step * 3);
+//             // setTimeout(() => setWindowState('closed'), step * 4);
+//             // setTimeout(() => setWindowState('idle'), step * 5);
+//             // setTimeout(() => setWindowState('open'), step * 6);
+//             setTimeout(() => setWindowState('open'), step);
+//             setTimeout(() => setWindowState('idle'), step * 2);
+//             setTimeout(() => setWindowState('closed'), step * 3);
+//             setTimeout(() => setWindowState('closed'), step * 4);
+//             setTimeout(() => setWindowState('idle'), step * 5);
+//             setTimeout(() => setWindowState('open'), step * 6);
+//         }, animationInterval);
 
-//       setTimeout(() => {
-//         setWindowState('close');
-//       }, step * 2); // At 33.34%
+//         return () => clearInterval(interval);
+//     }, []);
 
-//       setTimeout(() => {
-//         setWindowState('');
-//       }, step * 3); // At 50%
-
-//       setTimeout(() => {
-//         setWindowState('open');
-//       }, step * 4); // At 66.67%
-
-//       setTimeout(() => {
-//         setWindowState('close');
-//       }, step * 5); // At 83.34%
-
-//       setTimeout(() => {
-//         setWindowState('');
-//       }, step * 6); // At 100%
-//     }, animationInterval);
-
-//     return () => clearInterval(interval);
-//   }, []);
-
-//   return (
-   
-
-//     <div className="containers">
-//       <div className="background"></div>
-//       <div className={`window1 ${windowState}`}>
-//         <img src={img1} className="image image1" alt="Window 1" />
-//       </div>
-//       <div className={`window2 ${windowState}`}>
-//         <img src={img2} className="image image2" alt="Window 2" />
-//       </div>
-//       <div className={`window ${windowState}`}>
-//         <img src={img3} className="image image" alt="Window 3" />
-//       </div>
-//     </div>
-//   );
+//     return (
+//         <div className="containers">
+//             {/* <div className="background"></div> */}
+//             <div className={`window1 ${windowState}`}>
+//                 <img src={img1} className="image1" alt="Window 1" />
+//                 {/* <div className={`shutter ${windowState}`}></div> */}
+//             </div>
+//             <div className={`window2 ${windowState}`}>
+//                 <img src={img2} className="image2" alt="Window 2" />
+//                 <div className={`shutter ${windowState}`}></div>
+//             </div>
+//             <div className={`window ${windowState}`}>
+//                 <img src={img3} className="image" alt="Window 3" />
+//                 <div className={`shutter ${windowState}`}></div>
+//             </div>
+            
+//         </div>
+//     );
 // };
 
 // export default Animation;
 
-
-// changedd
 
 
 import React, { useEffect, useState } from 'react';
@@ -72,28 +60,19 @@ import './animation.css';
 import img1 from './images/img1.png';
 import img2 from './images/img2.png';
 import img3 from './images/img3.png';
-import { Col, Row } from 'react-bootstrap'
+
 const Animation = () => {
     const [windowState, setWindowState] = useState('closed');
 
     useEffect(() => {
-        const animationInterval = 12000; // 12 seconds for the complete animation cycle
-        const step = animationInterval / 6; // 6 steps
+        const animationInterval = 12000; 
+        const step = animationInterval / 6; 
 
         const interval = setInterval(() => {
             setWindowState('open');
-            // setTimeout(() => setWindowState('closed'), step);
-            // setTimeout(() => setWindowState('idle'), step * 2);
-            // setTimeout(() => setWindowState('open'), step * 3);
-            // setTimeout(() => setWindowState('closed'), step * 4);
-            // setTimeout(() => setWindowState('idle'), step * 5);
-            // setTimeout(() => setWindowState('open'), step * 6);
-            setTimeout(() => setWindowState('open'), step);
             setTimeout(() => setWindowState('idle'), step * 2);
             setTimeout(() => setWindowState('closed'), step * 3);
-            setTimeout(() => setWindowState('closed'), step * 4);
             setTimeout(() => setWindowState('idle'), step * 5);
-            setTimeout(() => setWindowState('open'), step * 6);
         }, animationInterval);
 
         return () => clearInterval(interval);
@@ -101,43 +80,20 @@ const Animation = () => {
 
     return (
         <div className="containers">
-            {/* <div className="background"></div> */}
-            <div className={`window1 ${windowState}`}>
-                <img src={img1} className="image1" alt="Window 1" />
+            <div className={`window ${windowState}`}>
+                <img src={img1} className="image" alt="Window 1" />
                 <div className={`shutter ${windowState}`}></div>
             </div>
-            <div className={`window2 ${windowState}`}>
-                <img src={img2} className="image2" alt="Window 2" />
+            <div className={`window ${windowState}`}>
+                <img src={img2} className="image" alt="Window 2" />
                 <div className={`shutter ${windowState}`}></div>
             </div>
             <div className={`window ${windowState}`}>
                 <img src={img3} className="image" alt="Window 3" />
                 <div className={`shutter ${windowState}`}></div>
             </div>
-            
         </div>
-
-        // <div className="containers">
-        //     <div className="background">
-        //     <Row className='container2  justify-content-md-center'>
-        //     <Col xs lg="2" className={`window1 ${windowState}`}>
-        //         <img src={img1} className="image1" alt="Window 1" />
-        //         <div className={`shutter ${windowState}`}></div>
-        //     </Col>
-        //     <Col xs lg="2" className={`window2 ${windowState}`}>
-        //         <img src={img2} className="image2" alt="Window 2" />
-        //         <div className={`shutter ${windowState}`}></div>
-        //     </Col>
-        //     <Col xs lg="2" className={`window ${windowState}`}>
-        //         <img src={img3} className="image" alt="Window 3" />
-        //         <div className={`shutter ${windowState}`}></div>
-        //     </Col>
-        //     </Row>
-        //     </div>
-        // </div>
     );
 };
 
 export default Animation;
-
-// responsive
